0606be1043e89b9205c3cc344264278dfa86b4c5
AlessiaCicca <alessia.ciccaglione02@gmail.com>
2026-05-23 12:32:03 +0000

    Update experiments

--- experiments/run_realData.py
+++ experiments/run_realData.py
@@ -31,6 +31,11 @@ import pandas as pd
 import torch
 import yaml
 from sklearn.preprocessing import OneHotEncoder
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(ROOT))
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
