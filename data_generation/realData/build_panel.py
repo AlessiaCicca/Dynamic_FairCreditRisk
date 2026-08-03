070a9f450c05c249589129ed64b6432b77b3f060
Alessia Ciccaglione <162203648+AlessiaCicca@users.noreply.github.com>
2026-08-03 08:40:25 +0200

    Update build_panel.py

--- data_generation/realData/build_panel.py
+++ data_generation/realData/build_panel.py
@@ -1,6 +1,5 @@
 """
 Build the longitudinal panel from Freddie Mac performance data + matched CSV.
-
 """
 
 import os
@@ -9,69 +8,25 @@ import glob
 import shutil
 import zipfile
 import argparse
-
 import numpy as np
 import pandas as pd
-
 import pyarrow as pa
 import pyarrow.parquet as pq
 
+# Input columns
+PERF_COLS_FULL = ["loan_sequence_number","monthly_reporting_period","current_upb","current_loan_delinquency_status","loan_age","remaining_months_to_maturity",
+    "defect_settlement_date","modifications_flag","zero_balance_code","zero_balance_effective_date","current_interest_rate","current_deferred_upb",
+    "due_date_last_paid_installment","mi_recoveries","net_sales_proceeds","non_mi_recoveries","expenses","legal_costs","maintenance_costs","taxes_and_insurance",
+    "miscellaneous_expenses","actual_loss","modification_cost","step_modification_flag","deferred_payment_plan","estimated_ltv","zero_balance_removal_upb",
+    "delinquent_accrued_interest","delinquency_due_to_disaster","borrower_assistance_status","current_month_modification_loss","interest_bearing_upb"]
 
-PERF_COLS_FULL = [
-    "loan_sequence_number",
-    "monthly_reporting_period",
-    "current_upb",
-    "current_loan_delinquency_status",
-    "loan_age",
-    "remaining_months_to_maturity",
-    "defect_settlement_date",
-    "modifications_flag",
-    "zero_balance_code",
-    "zero_balance_effective_date",
-    "current_interest_rate",
-    "current_deferred_upb",
-    "due_date_last_paid_installment",
-    "mi_recoveries",
-    "net_sales_proceeds",
-    "non_mi_recoveries",
-    "expenses",
-    "legal_costs",
-    "maintenance_costs",
-    "taxes_and_insurance",
-    "miscellaneous_expenses",
-    "actual_loss",
-    "modification_cost",
-    "step_modification_flag",
-    "deferred_payment_plan",
-    "estimated_ltv",
-    "zero_balance_removal_upb",
-    "delinquent_accrued_interest",
-    "delinquency_due_to_disaster",
-    "borrower_assistance_status",
-    "current_month_modification_loss",
-    "interest_bearing_upb",
-]
-
-# Columns to keep in output 
-PERF_COLS_KEEP = [
-    "loan_sequence_number",
-    "monthly_reporting_period",
-    "current_upb",
-    "current_loan_delinquency_status",
-    "loan_age",
-    "remaining_months_to_maturity",
-    "modifications_flag",
-    "zero_balance_code",
-    "zero_balance_effective_date",
-    "current_interest_rate",
-    "current_deferred_upb",
-    "estimated_ltv",
-    "delinquency_due_to_disaster",
-    "borrower_assistance_status",
-]
+# Output columns
+PERF_COLS_KEEP = ["loan_sequence_number", "monthly_reporting_period","current_upb","current_loan_delinquency_status","loan_age","remaining_months_to_maturity",
+    "modifications_flag","zero_balance_code","zero_balance_effective_date","current_interest_rate","current_deferred_upb","estimated_ltv","delinquency_due_to_disaster",
+    "borrower_assistance_status"]
 
 
-# Extract performance files 
+# Extract performance files from drive
 def extract_performance_zips(year, freddie_dir, perf_local):
     zip_pattern_sub  = os.path.join(freddie_dir, f"historical_data_{year}", f"historical_data_{year}Q*.zip")
     zip_pattern_flat = os.path.join(freddie_dir, f"historical_data_{year}Q*.zip")
@@ -79,7 +34,7 @@ def extract_performance_zips(year, freddie_dir, perf_local):
     zip_files = glob.glob(zip_pattern_sub) or glob.glob(zip_pattern_flat)
 
     if not zip_files:
-        raise FileNotFoundError(f"No zip files found for {year}." )
+        print(f"No zip files found for {year}." )
 
     extracted = []
     
@@ -99,25 +54,21 @@ def extract_performance_zips(year, freddie_dir, perf_local):
                 extracted.append(dest)
 
     if not extracted:
-        raise RuntimeError(f"No TIME files extracted for {year}.")
+        print(f"No TIME files extracted for {year}.")
     return extracted
 
 
 # Reads Freddie Mac performance files, keeps only loans present in the matched dataset, and saves them to a Parquet file.
-
 PARQUET_SCHEMA = pa.schema([(col, pa.string()) for col in PERF_COLS_KEEP])
 #Parquet is a file format for saving data tables to disk, like CSV, but designed for large datasets.
 
-def load_performance(perf_txt_files, loan_ids, parquet_path, chunk_size=200_000):
-    writer     = None
+def load_performance(perf_txt_files, loan_ids, parquet_path, chunk_size=200000):
+    writer = None
     for f in perf_txt_files:
         fname = os.path.basename(f)
         with open(f, "r") as fh:
             n_cols_file = len(fh.readline().split("|"))
-        for chunk in pd.read_csv(f, sep="|", header=None,
-                                  names=PERF_COLS_FULL[:n_cols_file],
-                                  dtype=str, low_memory=False,
-                                  chunksize=chunk_size, on_bad_lines="skip"):
+        for chunk in pd.read_csv(f, sep="|", header=None, names=PERF_COLS_FULL[:n_cols_file],dtype=str, low_memory=False,chunksize=chunk_size, on_bad_lines="skip"):
             # Keeps only rows belonging to loans present in the matched dataset
             filtered = chunk[chunk["loan_sequence_number"].isin(loan_ids)]
             if filtered.empty:
@@ -125,8 +76,7 @@ def load_performance(perf_txt_files, loan_ids, parquet_path, chunk_size=200_000)
             # Selects only the useful columns and fills missing ones with NaN     
             filtered = filtered.reindex(columns=PERF_COLS_KEEP)
             # Converts the chunk to Parquet format 
-            table = pa.Table.from_pandas(filtered, schema=PARQUET_SCHEMA,
-                                          preserve_index=False)
+            table = pa.Table.from_pandas(filtered, schema=PARQUET_SCHEMA,preserve_index=False)
             if writer is None:
                 writer = pq.ParquetWriter(parquet_path, PARQUET_SCHEMA)
             writer.write_table(table)
@@ -143,7 +93,7 @@ def load_performance(perf_txt_files, loan_ids, parquet_path, chunk_size=200_000)
     return perf
 
 
-
+# Create some time columns that could be useful to the analysis of the dataset
 def add_time_columns(perf):
     perf = perf.copy()
     
@@ -151,27 +101,30 @@ def add_time_columns(perf):
     perf["period_year"] = perf["monthly_reporting_period"].str[:4].astype(int)
     perf["period_month"] = perf["monthly_reporting_period"].str[4:6].astype(int)
     perf["quarter"] = ((perf["period_month"] - 1) // 3 + 1).astype(int)
-
     perf["period_quarter"] = ( perf["period_year"].astype(str) + "Q" + perf["quarter"].astype(str))
 
-    for col in [
-        "current_upb",  "loan_age",  "remaining_months_to_maturity",
-        "current_interest_rate", "estimated_ltv", "current_deferred_upb",]:
+    numeric_columns=["current_upb",  "loan_age",  "remaining_months_to_maturity","current_interest_rate", "estimated_ltv", "current_deferred_upb"]
+    for col in numeric_columns:
         if col in perf.columns:
             perf[col] = pd.to_numeric(perf[col], errors="coerce")
 
 
-    # FirstEventTime definition
-    perf["_is_default"] = (
-    perf["current_loan_delinquency_status"].str.strip()
-    .isin({"", "0", "00"})
-    .map({True: 0, False: 1}))
+    # Default Event definition:
+    # Remove space and special characters 
+    status = perf["current_loan_delinquency_status"].str.strip()
+    # Create MAP
+    is_current = status.isin({"", "0", "00"})
+    
+    perf["_is_default"] = is_current.map({True: 0,False: 1})
+    
 
-    first_event = (
-        perf[perf["_is_default"] == 1]
-        .groupby("loan_sequence_number")["loan_age"]
-        .min()
-        .rename("FirstEventTime"))
+    # FirstEventTime definition
+    default_loans = perf[perf["_is_default"] == 1]
+    grouped = default_loans.groupby("loan_sequence_number")
+    first_event = grouped["loan_age"].min()
+    
+    # Series rename
+    first_event = first_event.rename("FirstEventTime")
 
     perf = perf.merge(first_event, on="loan_sequence_number", how="left") 
     perf.drop(columns=["_is_default"], inplace=True)
@@ -185,19 +138,14 @@ def build_panel(perf, matched):
     gc.collect()
     dups = panel.duplicated(subset=["loan_sequence_number", "monthly_reporting_period"]).sum()
     if dups > 0:
-        print(f"WARNING: {dups:,} duplicate rows on (loan, month).")
+        print(f"Duplicate rows on (loan, month).")
     return panel
 
 # Order columns and save
 def save_panel(panel, matched, output_path):
-    id_cols   = ["loan_sequence_number", "period_quarter", "period_year",
-                 "quarter", "monthly_reporting_period"]
-    perf_cols = ["loan_age", "remaining_months_to_maturity",
-                 "current_upb", "current_interest_rate", "estimated_ltv",
-                 "current_deferred_upb", "current_loan_delinquency_status",
-                 "modifications_flag", "zero_balance_code",
-                 "zero_balance_effective_date",
-                 "delinquency_due_to_disaster", "borrower_assistance_status"]
+    id_cols   = ["loan_sequence_number", "period_quarter", "period_year","quarter", "monthly_reporting_period"]
+    perf_cols = ["loan_age", "remaining_months_to_maturity", "current_upb", "current_interest_rate", "estimated_ltv", "current_deferred_upb", "current_loan_delinquency_status",
+                 "modifications_flag", "zero_balance_code","zero_balance_effective_date","delinquency_due_to_disaster", "borrower_assistance_status"]
     target_cols=[]
     origination_cols = [c for c in matched.columns
                         if c != "loan_sequence_number"
@@ -247,14 +195,8 @@ def run_build_panel(year, drive_root):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--drive_root", required=True,
-        help="Root directory (e.g. /content/drive/MyDrive/thesis_data)"
-    )
-    parser.add_argument(
-        "--year", type=int, required=True,
-        help="Year to process (e.g. 2022)"
-    )
+    parser.add_argument("--drive_root", required=True)
+    parser.add_argument("--year", type=int, required=True)
     args = parser.parse_args()
 
     run_build_panel(year=args.year, drive_root=args.drive_root)
